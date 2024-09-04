e9ba9f6fa560716a72c06997dc12d40c5e04153c
luchocalvani <luchocalvani@gmail.com>
2024-09-04 10:36:32 -0300

    entrega

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,6 +1,43 @@
 package com.template;
 
 public class Elevator {
+    int min;
+    int max;
+    int capacity;
+    private int currentFloor;
+    private int currentPassenger;
+
+    public Elevator(int min, int max, int capacity) {
+        this.min = min;
+        this.max = max;
+        this.capacity = capacity;
+    }
+
+    public void moveToFloor(int floor){
+        if (floor >= min && floor <= max){
+            currentFloor = floor;
+        }
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void addPassenger(){
+        if (currentPassenger < capacity){
+            currentPassenger += 1;
+        }
+    }
+
+    public int getPassengerCount() {
+        return currentPassenger;
+    }
+
+    public void removePassenger(){
+        if (currentPassenger > 0){
+            currentPassenger -= 1;
+        }
+    }
 
 
 
